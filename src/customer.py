7ff9ad64f044daedfb0f81a683ea1661641aeb89
colette dufficy <c.dufficy@gmail.com>
2021-10-20 12:08:04 +0100

    running initial tests

---
+++ src/customer.py
@@ -0,0 +1,4 @@
+class CustomerDetails:
+    def __init__(self, name, wallet):
+        self.name = name
+        self.wallet = wallet
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,10 +1,21 @@
 import unittest
 from src.pub import Pub
+from src.customer import CustomerDetails
+
 
 class TestPub(unittest.TestCase):
     def setUp(self):
-        self.pub = Pub("The Prancing Pony")
+        self.pub = Pub("The Prancing Pony", 1000)
+        self.customer = CustomerDetails("Iain", 50)
     
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
+        
+    def test_till_total_cash(self):
+        self.assertEqual(1000, self.pub.till)
 
+    def test_customer_has_name(self):
+        self.assertEqual("Iain", self.customer.name)
+        
+    def test_customer_has_money(self):
+        self.assertEqual(50, self.customer.wallet)
